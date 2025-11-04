7526fabe805b1ac1d1bdcbbe20c03f448d20d384
Fredrick <freddyjames534@gmail.com>
2025-11-04 18:35:16 +0300

    refactor: remove unused heading text from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-background">
       <h1 className="text-5xl text-whitefont-semibold tracking-tight">
-        Technofusion
       </h1>
     </main>
   );
